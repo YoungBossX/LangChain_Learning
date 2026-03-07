e099d72a9230123d1043519bfeb9f31f15dea573
Xie Caicheng <87680698+YoungBossX@users.noreply.github.com>
2026-03-07 13:45:35 +0800

    commit

--- application/Agent项目/agent/tools/agent_tools.py
+++ application/Agent项目/agent/tools/agent_tools.py
@@ -1,4 +1,8 @@
 import os
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).parent.parent.parent))
+
 from utils.logger_handler import logger
 from langchain_core.tools import tool
 from rag.rag_service import RagSummarizeService
@@ -106,3 +110,6 @@ def fetch_external_data(user_id: str, month: str) -> str:
 @tool(description="无入参，无返回值，调用后触发中间件自动为报告生成的场景动态注入上下文信息，为后续提示词切换提供上下文信息")
 def fill_context_for_report():
     return "fill_context_for_report已调用"
+
+# if __name__ == '__main__':
+#     print(fetch_external_data("1001", "2025-01"))
